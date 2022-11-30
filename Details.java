158473dff8f6ffbfd816e0bbd1edd19e521f68f7
Abhishek Singh <97419930+Shawn1frost@users.noreply.github.com>
2022-11-30 23:02:25 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,17 @@
+package App;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author abhi1
+ */
+public class Details extends javax.swing.JFrame {
+    //Global Variables
+    public static String usernameGlobal="username";
+    public static int cS=0, bS=0;
+}
